0e7467653ca69291301023ddec87f6f25339d88d
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2025-08-29 09:51:59 +0530

    Add files via upload

---
+++ langgraph/agent_w_memory.py
@@ -0,0 +1,65 @@
+from langchain_core.messages import SystemMessage
+from langchain_openai import ChatOpenAI
+
+from langgraph.graph import START, StateGraph, MessagesState
+from langgraph.prebuilt import tools_condition, ToolNode
+
+def add(a: int, b: int) -> int:
+    """Adds a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a + b
+
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+def divide(a: int, b: int) -> float:
+    """Divide a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a / b
+
+tools = [add, multiply, divide]
+
+# Define LLM with bound tools
+llm = ChatOpenAI(model="llama3.1", openai_api_base="http://localhost:11434/v1", api_key="ollama")
+llm_with_tools = llm.bind_tools(tools)
+
+# System message
+sys_msg = SystemMessage(content="You are a helpful assistant tasked with writing performing arithmetic on a set of inputs.")
+
+# Node
+def assistant(state: MessagesState):
+   return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+
+# Compile graph with memory
+from langgraph.checkpoint.memory import MemorySaver
+memory = MemorySaver()
+graph = builder.compile(checkpointer=memory)
+config = {"configurable": {"thread_id": "1"}}
+# invoke with the config to resume with earlier saved state
